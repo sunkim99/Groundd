a2965d942f4ea58e4e0b701de6a90ea1c95fbe84
SUN <dlrlvlrl12@naver.com>
2020-04-27 01:33:19 +0900

    427-1

--- app/src/main/java/com/example/ground/MainActivity.java
+++ app/src/main/java/com/example/ground/MainActivity.java
@@ -8,42 +8,55 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button btn_school, btn_board, btn_character, btn_event, btn_game; //btn_setting;
+    Button btn_school, btn_board, btn_character, btn_event, btn_game, btn_setting;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         btn_school = (Button) findViewById(R.id.btn_school);
-        btn_school.setOnClickListener(this);
-
-
-
         btn_board = findViewById(R.id.btn_board);
         btn_character = findViewById(R.id.btn_character);
         btn_event =  findViewById(R.id.btn_event);
         btn_game = findViewById(R.id.btn_game);
-        //btn_setting = findViewById(R.id.btn_setting);
+        btn_setting = findViewById(R.id.btn_setting);
 
 
+        btn_school.setOnClickListener(this);
         btn_board.setOnClickListener(this);
         btn_character.setOnClickListener(this);
         btn_event.setOnClickListener(this);
         btn_game.setOnClickListener(this);
-        // btn_setting.setOnClickListener(this);
+        btn_setting.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.btn_school) {
+        if (v.getId() == R.id.btn_school) { //학교 게시판
             Intent intent1 = new Intent(MainActivity.this, school_information.class);
             startActivity(intent1);
         }
-
-        if (v.getId() == R.id.btn_character) {
-            Intent intent2 = new Intent(MainActivity.this, CharInven.class);
+        if (v.getId() == R.id.btn_board) { // 게시판
+            Intent intent2 = new Intent(MainActivity.this, ForumForum.class);
             startActivity(intent2);
         }
-
+        if (v.getId() == R.id.btn_character) { // 내 캐릭터
+            Intent intent3 = new Intent(MainActivity.this, CharInven.class);
+            startActivity(intent3);
+        }
+        if (v.getId() == R.id.btn_event) { // 공지 이벤트
+            Intent intent4 = new Intent(MainActivity.this, NoticeNotice.class);
+            startActivity(intent4);
+        }
+        /*if (v.getId() == R.id.btn_game) { // 게임
+            Intent intent5 = new Intent(MainActivity.this, NoticeEvent.class);
+            startActivity(intent5);
+        }*/
+        if (v.getId() == R.id.btn_setting) { // 설정
+            Intent intent6 = new Intent(MainActivity.this, Config.class);
+            startActivity(intent6);
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/ground/NoticeEvent.java
@@ -0,0 +1,14 @@
+package com.example.ground;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoticeEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice_event);
+    }
+}
