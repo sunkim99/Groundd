3c7b3f8e6aa4b886d4a32ac53ae78d43ca62a57c
NEO-LUMIX <NEO-LUMIX@users.noreply.github.com>
2020-04-26 22:38:48 +0900

    클래스 3개만 추가시켰음

---
+++ app/src/main/java/com/example/ground/forum_forum.java
@@ -0,0 +1,14 @@
+package com.example.ground;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class forum_forum extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forum_forum);
+    }
+}
---
+++ app/src/main/java/com/example/ground/forum_forum_write.java
@@ -0,0 +1,14 @@
+package com.example.ground;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class forum_forum_write extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forum_forum_write);
+    }
+}
