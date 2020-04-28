807e8764bf18643b098503032fa97a9fa3717bfd
NEO-LUMIX <NEO-LUMIX@users.noreply.github.com>
2020-04-28 20:46:59 +0900

    0428 _ 포럼쪽 상호호환 기능 추가중

--- app/src/main/java/com/example/ground/forum_forum.java
+++ app/src/main/java/com/example/ground/forum_forum.java
@@ -2,13 +2,44 @@ package com.example.ground;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class forum_forum extends AppCompatActivity {
+public class forum_forum extends AppCompatActivity implements View.OnClickListener{
+
+    Button next_tab;
+    Button write;
+    Button cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forum_forum);
+
+        next_tab = (Button) findViewById(R.id.go_forum_image);
+        write = (Button) findViewById(R.id.go_forum_write);
+        cancel = (Button) findViewById(R.id.btn_forum_forum_cancel);
+
+        next_tab.setOnClickListener(this);
+        write.setOnClickListener(this);
+        cancel.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.go_forum_image) {
+            Intent intent01 = new Intent( forum_forum.this, forum_image.class);
+            startActivity(intent01);
+        }
+        if(v.getId() == R.id.go_forum_write) {
+            Intent intent02 = new Intent( forum_forum.this, forum_forum_write.class);
+            startActivity(intent02);
+        }
+        if(v.getId() == R.id.btn_forum_forum_cancel) {
+            Intent intent03 = new Intent( forum_forum.this, MainActivity.class);
+            startActivity(intent03);
+        }
     }
 }
