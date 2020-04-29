0f25ee6880dd87927e90ed21c88d893a81c257e7
SUN <dlrlvlrl12@naver.com>
2020-04-29 22:28:58 +0900

    42901

--- app/src/main/java/com/example/ground/forum_forum.java
+++ app/src/main/java/com/example/ground/forum_forum.java
@@ -1,11 +1,12 @@
 package com.example.ground;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 //게시판
 public class forum_forum extends AppCompatActivity implements View.OnClickListener{
 
@@ -28,11 +29,12 @@ public class forum_forum extends AppCompatActivity implements View.OnClickListen
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.go_forum_image) {
+        if(v.getId() == R.id.go_forum_image) { //그림게시판이동
             Intent intent01 = new Intent( forum_forum.this, forum_image.class);
             startActivity(intent01);
         }
-        if(v.getId() == R.id.go_forum_write) {
+        if(v.getId() == R.id.go_forum_write) { //글쓰기 버튼 누름
+            Log.d("TEST1234", "글작성 버튼 눌림");
             Intent intent02 = new Intent( forum_forum.this, forum_forum_write.class);
             startActivity(intent02);
         }
