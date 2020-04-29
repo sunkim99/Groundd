c5a0cd1c742e265b43f0dcff1f04fe86a2373ad8
NEO-LUMIX <NEO-LUMIX@users.noreply.github.com>
2020-04-29 22:21:14 +0900

    0429 _ fragment 테스트중

---
+++ app/src/main/java/com/example/ground/forum_forum_ex_fragment.java
@@ -0,0 +1,51 @@
+/*
+package com.example.ground;
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+//게시판
+public class forum_forum_ex_fragment extends Fragment implements View.OnClickListener{
+
+    Button next_tab, write, cancel;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        next_tab = next_tab.findViewById(R.id.go_forum_image);
+        write = write.findViewById(R.id.go_forum_write);
+        cancel = cancel.findViewById(R.id.btn_forum_forum_cancel);
+
+        next_tab.setOnClickListener(this);
+        write.setOnClickListener(this);
+        cancel.setOnClickListener(this);
+        return inflater.inflate(R.layout.activity_forum_forum, container, false);
+
+    }
+
+    @Override
+    public void onClick (View v){
+        if (v.getId() == R.id.go_forum_image) {
+            Intent intent01 = new Intent(forum_forum.this, forum_image.class);
+            startActivity(intent01);
+        }
+        if (v.getId() == R.id.go_forum_write) {
+            Intent intent02 = new Intent(forum_forum.this, forum_forum_write.class);
+            startActivity(intent02);
+        }
+        if (v.getId() == R.id.btn_forum_forum_cancel) {
+            finish();
+        }
+    }
+}
+*/
