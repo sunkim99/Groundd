78fd70779e3ca26b8a2bf9f61538ed659271ca8a
NEO-LUMIX <NEO-LUMIX@users.noreply.github.com>
2020-05-10 22:26:15 +0900

    0510 - 로딩화면 애니메이션 추가(추후 수정)

--- app/src/main/java/com/example/ground/SplashActivity.java
+++ app/src/main/java/com/example/ground/SplashActivity.java
@@ -2,23 +2,42 @@ package com.example.ground;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.loading);
 
-        try {
-            Thread.sleep(3000); //대기 초 설정
-            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
-            finish();
-        } catch (Exception e) {
+        final ImageView iv = (ImageView)findViewById(R.id.logo_image);
+
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.logo);
+        iv.startAnimation(animation);
+
+        Handler hd = new Handler();
+        hd.postDelayed(new splashhandler(), 3000);
+
+    }
+
+    private class splashhandler implements Runnable{
+        public void run(){
+            startActivity(new Intent(getApplication(), LoginActivity.class));
+            SplashActivity.this.finish();
         }
     }
+
+    @Override
+    public void onBackPressed() {
+        //초반 플래시 화면에서 넘어갈때 뒤로가기 버튼 못누르게 함
+    }
 }
+
