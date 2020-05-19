2f72c413bbcc1bfecee7685a46acc0328b74448e
SUN <dlrlvlrl12@naver.com>
2020-05-19 21:05:46 +0900

    519 게임메인 타이머추가

--- app/src/main/java/com/example/ground/MainActivity.java
+++ app/src/main/java/com/example/ground/MainActivity.java
@@ -13,15 +13,6 @@ import android.widget.TextView;
 
 
 
-/*봉근오빠 이메세지보이시면 밑에 똑같이 답장한번 달아서 깃 올려주세욤
-*
-*
-*
-* */
-
-
-
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button btn_school, btn_board, btn_character, btn_event, btn_game, btn_setting;
     TextView et;
--- app/src/main/java/com/example/ground/game_main.java
+++ app/src/main/java/com/example/ground/game_main.java
@@ -3,6 +3,7 @@ package com.example.ground;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -14,17 +15,32 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 //오락메인
 public class game_main extends AppCompatActivity implements View.OnClickListener {
 
     Button go_other_menu, btn_gugu;
     Button cancel;
-    TextView count;
+    private TextView count;
+    private int counttime = 10;
+    private Handler handler = new Handler();
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
 
+            counttime -= 1;
+            count.setText(counttime+"");//1초 간격
 
-    private static final String FORMAT = "%02d:%02d:%02d";
-    int seconds, minutes;
+            if (counttime <= 1) {
+                handler.removeCallbacks(runnable);
+            } else {
+                handler.postDelayed(runnable, 1000);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,14 +50,15 @@ public class game_main extends AppCompatActivity implements View.OnClickListener
         go_other_menu = (Button) findViewById(R.id.go_game_ranking);
 
         btn_gugu = findViewById(R.id.btn_gugu);
-        count = findViewById(R.id.popup_multiplication_count);
+
         go_other_menu.setOnClickListener(this);
 
         btn_gugu.setOnClickListener(this);
 
 
-}
-
+        count = findViewById(R.id.tttt);
+        handler.post(runnable);
+    }
 
 
     public void onClick(View v) {
@@ -64,9 +81,11 @@ public class game_main extends AppCompatActivity implements View.OnClickListener
     private int firstNumber;
     private int secondNumber;
 
+
     private final void setEntertainmentFunction() {
         ((Button) findViewById(R.id.btn_gugu)).setOnClickListener(
                 (View.OnClickListener) (new View.OnClickListener() {
+
                     @Override
                     public void onClick(View v) {// 오락버튼 눌렀을 때
                         multiplicationView = new MultiplicationView(); // 객체 삽입
@@ -76,24 +95,13 @@ public class game_main extends AppCompatActivity implements View.OnClickListener
                         multiplicationView.showPopupView(); // popupview 생성
 
 
-                       /* CountDownTimer countDownTimer = new CountDownTimer(30000, 1000) {
-                            public void onTick(long millisUntilFinished) {
-                                count.setText(String.format(Locale.getDefault(), "%d sec.", millisUntilFinished / 1000L));
-                            }
-
-                            public void onFinish() {
-                                count.setText("Done.");
-                            }
-                        }.start();
-*/
-
-                        //카운트 다운되는 코드 여기에 추가해봐야할듯..
 
                         Log.d("TEST1234", "스레드 진행1" + Thread.currentThread());
                         multiplicationView.popupTextView().setText(multiplicationQuestion());
                         multiplicationView.popupEdittext().addTextChangedListener((TextWatcher) (new TextWatcher() {
                             @Override
                             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
                             }
 
                             @Override
@@ -103,7 +111,8 @@ public class game_main extends AppCompatActivity implements View.OnClickListener
                             @Override
                             public void onTextChanged(CharSequence s, int start, int before, int count) {
                                 try {
-                                    String answer = String.valueOf(s);
+                                    String answer = multiplicationView.popupEdittext().getText().toString();
+                                    //String answer = String.valueOf(s);
                                     int intConverter = Integer.parseInt(answer);
                                     if (firstNumber * secondNumber == intConverter) {
                                         multiplicationView.dismiss(); //정답이 일치할경우 mulirplicationView에 dismiss 클래스 실행 -> 팝업뷰가 사라짐 로그 뜨면서 팝업창 사라짐
