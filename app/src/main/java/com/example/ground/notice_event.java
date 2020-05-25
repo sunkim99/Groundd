a6b55e9ad6e18bd7b3726b8ee7a19b5709ff9d0e
NEO-LUMIX <NEO-LUMIX@users.noreply.github.com>
2020-05-25 13:16:12 +0900

    0525 - 버튼 상호작용 추가

--- app/src/main/java/com/example/ground/notice_event.java
+++ app/src/main/java/com/example/ground/notice_event.java
@@ -2,13 +2,31 @@ package com.example.ground;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class notice_event extends AppCompatActivity {
+public class notice_event extends AppCompatActivity implements View.OnClickListener{
+
+    Button go_notice_notice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice_event);
+
+        go_notice_notice = findViewById(R.id.go_notice_event);
+
+
+        go_notice_notice.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.go_notice_notice) {
+            Intent intent01 = new Intent(notice_event.this, notice_notice.class);
+            startActivity(intent01);
+        }
     }
 }
--- app/src/main/java/com/example/ground/notice_notice.java
+++ app/src/main/java/com/example/ground/notice_notice.java
@@ -2,13 +2,31 @@ package com.example.ground;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class notice_notice extends AppCompatActivity {
+public class notice_notice extends AppCompatActivity implements View.OnClickListener{
+
+    Button go_notice_event;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice_notice);
+
+        go_notice_event= findViewById(R.id.go_notice_event);
+
+
+        go_notice_event.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.go_notice_event) {
+            Intent intent01 = new Intent(notice_notice.this, notice_event.class);
+            startActivity(intent01);
+        }
     }
 }
