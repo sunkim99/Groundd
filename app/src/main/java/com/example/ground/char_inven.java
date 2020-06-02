1ec9f719fcdb62bc98e21744147d88e615ef6766
NEO-LUMIX <NEO-LUMIX@users.noreply.github.com>
2020-06-03 00:02:49 +0900

    0603 - 캐릭터 화면 구현을 위한 팝업창 구현중

--- app/src/main/java/com/example/ground/char_inven.java
+++ app/src/main/java/com/example/ground/char_inven.java
@@ -11,19 +11,33 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class char_inven extends AppCompatActivity implements View.OnClickListener{
+public class char_inven extends AppCompatActivity implements View.OnClickListener {
     Button ex_char1, ex_char2;
     TextView show_nick;
     ImageView MY_char;
     Button top_navi, btn_setting;
+    Button btn_head, btn_hat, btn_face, btn_acce, btn_bg, btn_cloth;
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
+    TextView txtResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_char_inven);
         top_navi = findViewById(R.id.top_navi);
         btn_setting = findViewById(R.id.btn_setting);
+        btn_head = findViewById(R.id.btn_head);
+        btn_head.setOnClickListener(this);
+        btn_hat = findViewById(R.id.btn_hat);
+        btn_hat.setOnClickListener(this);
+        btn_face = findViewById(R.id.btn_face);
+        btn_face.setOnClickListener(this);
+        btn_acce = findViewById(R.id.btn_acce);
+        btn_acce.setOnClickListener(this);
+        btn_bg = findViewById(R.id.btn_bg);
+        btn_bg.setOnClickListener(this);
+        btn_cloth = findViewById(R.id.btn_cloth);
+        btn_cloth.setOnClickListener(this);
         top_navi.setOnClickListener(this);
         btn_setting.setOnClickListener(this);
 
@@ -35,21 +49,20 @@ public class char_inven extends AppCompatActivity implements View.OnClickListene
         show_nick = findViewById(R.id.nickname_view);
         show_nick.setText(temp01);
 
-        ex_char1 =  findViewById(R.id.ex_char1);
-        ex_char2 =  findViewById(R.id.ex_char2);
+        ex_char1 = findViewById(R.id.ex_char1);
+        ex_char2 = findViewById(R.id.ex_char2);
 
         ex_char1.setOnClickListener(this);
         ex_char2.setOnClickListener(this);
 
         MY_char = findViewById(R.id.MY_char);
-        if (MY_Char_head == 0){
+        if (MY_Char_head == 0) {
             MY_char.setImageResource(R.drawable.ex_char1);
-        }
-        else if (MY_Char_head == 1){
+        } else if (MY_Char_head == 1) {
             MY_char.setImageResource(R.drawable.ex_char2);
         }
 
-}
+    }
 
 
     @Override
@@ -59,19 +72,17 @@ public class char_inven extends AppCompatActivity implements View.OnClickListene
 
         if (v.getId() == R.id.ex_char1) {
             Char_head.setChar_head(0);
-            if (MY_Char_head == 0){
+            if (MY_Char_head == 0) {
                 MY_char.setImageResource(R.drawable.ex_char1);
-            }
-            else if (MY_Char_head == 1){
+            } else if (MY_Char_head == 1) {
                 MY_char.setImageResource(R.drawable.ex_char2);
             }
         }
         if (v.getId() == R.id.ex_char2) {
             Char_head.setChar_head(1);
-            if (MY_Char_head == 0){
+            if (MY_Char_head == 0) {
                 MY_char.setImageResource(R.drawable.ex_char1);
-            }
-            else if (MY_Char_head == 1){
+            } else if (MY_Char_head == 1) {
                 MY_char.setImageResource(R.drawable.ex_char2);
             }
         }
@@ -84,6 +95,24 @@ public class char_inven extends AppCompatActivity implements View.OnClickListene
         if (v.getId() == R.id.btn_setting) { // 설정
             Intent intent03 = new Intent(char_inven.this, configActivity.class);
             startActivity(intent03);
+        }
+
+        if (v.getId() == R.id.btn_head) { // 설정
+            Intent intent = new Intent(this, char_inven_head.class);
+            intent.putExtra("data", "Test Popup");
+            startActivityForResult(intent, 1);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                //데이터 받기
+                String result = data.getStringExtra("result");
+                txtResult.setText(result);
+            }
         }
     }
 }
