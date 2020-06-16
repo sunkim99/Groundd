7f3a76dd7e291893154635096414f8368b3f0466
brisela <kretixe@gmail.com>
2020-06-16 21:20:12 +0900

    사용자 게시글 댓글 리스트

--- app/src/main/java/com/example/ground/config_my_forum_forum.java
+++ app/src/main/java/com/example/ground/config_my_forum_forum.java
@@ -43,9 +43,9 @@ public class config_my_forum_forum extends AppCompatActivity implements View.OnC
     private static final String TAG_commCon = "commCon";
     private static final String TAG_commDate = "commDate";
 
-    boolean clickednum = true;
+    boolean clickednum;
 
-    ListView noticelist, commentlist;
+    ListView list;
     ArrayList<HashMap<String, String>> MyNoticeList;
     ArrayList<HashMap<String, String>> MyCommentList;
     String mJsonString;
@@ -65,7 +65,6 @@ public class config_my_forum_forum extends AppCompatActivity implements View.OnC
         allround ID = (allround) getApplicationContext(); // 전역변수 ID 소환
         String userID = ID.getID();
 
-
         tvID=findViewById(R.id.tvID);
         tvID.setText(userID);
         Log.d("TEST1234", "[Config] 쓰레드확인1:"+userID);
@@ -75,17 +74,50 @@ public class config_my_forum_forum extends AppCompatActivity implements View.OnC
 
 
         //리스트뷰 정의
-        noticelist = (ListView) findViewById(R.id.config_list);
-        commentlist = (ListView) findViewById(R.id.config_list);
+        list = (ListView) findViewById(R.id.config_list);
         MyNoticeList = new ArrayList<>();
         MyCommentList = new ArrayList<>();
 
-        if(clickednum = true) {
+        showmynot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                allround ID = (allround) getApplicationContext();
+                String userID = ID.getID();
+
+                clickednum = true;
+
+                config_my_forum_forum.MyNotice task = new config_my_forum_forum.MyNotice();
+                task.execute("http://olivia7626.dothome.co.kr/config_mynoticelist.php?userID=" + userID);
+                Log.d("TEST1324", "전당된 유저아이디 " + userID);
+            }
+        });
+        showmycomm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                allround ID = (allround) getApplicationContext();
+                String userID = ID.getID();
+
+                clickednum = false;
+
+                config_my_forum_forum.MyNotice task = new config_my_forum_forum.MyNotice();
+                task.execute("http://olivia7626.dothome.co.kr/config_mycommentlist.php?userID=" + userID);
+                Log.d("TEST1324", "전당된 유저아이디 " + userID);
+            }
+        });
+
+
+      /*  if (clickednum = true) {
             config_my_forum_forum.MyNotice task = new config_my_forum_forum.MyNotice();
             task.execute("http://olivia7626.dothome.co.kr/config_mynoticelist.php?userID=" + userID);
             Log.d("TEST1324", "전당된 유저아이디 " + userID);
-
-            noticelist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        }
+        else {
+            config_my_forum_forum.MyNotice task = new config_my_forum_forum.MyNotice();
+            task.execute("http://olivia7626.dothome.co.kr/config_mycommentlist.php?userID=" + userID);
+            Log.d("TEST1324", "전당된 유저아이디 " + userID);
+        }
+*/
+            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView parent, View v, int position, long id) {
                     Intent intent = new Intent(getApplicationContext(), config_my_forum_in.class); //클릭하면 이동하는 화면
@@ -99,27 +131,6 @@ public class config_my_forum_forum extends AppCompatActivity implements View.OnC
                     startActivity(intent);
                 }
             });
-        }
-        else {
-            config_my_forum_forum.MyComment task = new config_my_forum_forum.MyComment();
-            task.execute("http://olivia7626.dothome.co.kr/config_mycommentlist.php?userID=" + userID);
-            Log.d("TEST1324", "전당된 유저아이디 " + userID);
-
-            commentlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView parent, View view, int position, long id) {
-                    Intent comment_intent = new Intent(getApplicationContext(), config_my_forum_in.class);
-                    HashMap check_position = MyCommentList.get(position);
-
-                    Log.d("TEST1234", "게시판 글번호 " + check_position.get(TAG_notNum));
-
-                    String i = (String) check_position.get(TAG_notNum);
-                    comment_intent.putExtra("check_position2", i);
-
-                    startActivity(comment_intent);
-                }
-            });
-        }
 
     }
 
@@ -141,7 +152,7 @@ public class config_my_forum_forum extends AppCompatActivity implements View.OnC
 
                 progressDialog.dismiss();
                 mJsonString = result;
-                if(clickednum = true){
+                if(clickednum == true){
                 showMyNotice();
                 }
                 else{
@@ -197,10 +208,12 @@ public class config_my_forum_forum extends AppCompatActivity implements View.OnC
         }
         ///
         private void showMyNotice(){
+        MyNoticeList.clear();
         //allround ID = (allround) getApplicationContext(); // 전역변수 ID 소환
            // String userID = ID.getID();
             try {
                 JSONObject jsonObject = new JSONObject(mJsonString);
+                Log.d("가져온 notice json 데이터 : ", String.valueOf(jsonObject));
                 JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
 
                 //HashMap<String, String> hashMap1 = new HashMap<>();
@@ -229,7 +242,7 @@ public class config_my_forum_forum extends AppCompatActivity implements View.OnC
                         new String[]{TAG_notNum,TAG_notTi,TAG_notDate},
                         new int[]{R.id.textView_list_config_notNum, R.id.textView_list_config_notTi,R.id.textView_list_config_notDate}
                 );
-                noticelist.setAdapter(adapter);
+                list.setAdapter(adapter);
 
 
 
@@ -240,6 +253,39 @@ public class config_my_forum_forum extends AppCompatActivity implements View.OnC
 
     }
 
+    private void ShowMyComment() {
+        MyCommentList.clear();
+        try {
+            JSONObject jsonObject = new JSONObject(mJsonString);
+            Log.d("가져온 comment json 데이터 : ", String.valueOf(jsonObject));
+            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
+
+            for (int i =0 ; i < jsonArray.length(); i++){
+                JSONObject item = jsonArray.getJSONObject(i);
+
+                String notNum = item.getString(TAG_notNum);
+                String commCon = item.getString(TAG_commCon);
+                String commDate = item.getString(TAG_commDate);
+
+                HashMap<String, String> hashMap = new HashMap<>();
+
+                hashMap.put(TAG_notNum,notNum);
+                hashMap.put(TAG_commCon,commCon);
+                hashMap.put(TAG_commDate,commDate);
+
+                MyCommentList.add(hashMap);
+            }
+            ListAdapter adapter = new SimpleAdapter(
+                    config_my_forum_forum.this, MyCommentList,R.layout.config_item_list,
+                    new String[]{TAG_notNum,TAG_commCon,TAG_commDate},
+                    new int[]{R.id.textView_list_config_notNum,R.id.textView_list_config_notTi,R.id.textView_list_config_notDate}
+            );
+            list.setAdapter(adapter);
+        } catch (JSONException e) {
+            Log.d(TAG, "showResult :", e);
+        }
+    }
+    /*
     //사용자가 쓴 댓글 불러오기
     public class MyComment extends AsyncTask<String, Void, String>{
         ProgressDialog progressDialog;
@@ -306,38 +352,7 @@ public class config_my_forum_forum extends AppCompatActivity implements View.OnC
             return null;
         }
     }
-
-    private void ShowMyComment() {
-        try {
-            JSONObject jsonObject = new JSONObject(mJsonString);
-            Log.d("가져온 json 데이터 : ", String.valueOf(jsonObject));
-            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
-
-            for (int i =0 ; i < jsonArray.length(); i++){
-                JSONObject item = jsonArray.getJSONObject(i);
-
-                String notNum = item.getString(TAG_notNum);
-                String commCon = item.getString(TAG_commCon);
-                String commDate = item.getString(TAG_commDate);
-
-                HashMap<String, String> hashMap = new HashMap<>();
-
-                hashMap.put(TAG_notNum,notNum);
-                hashMap.put(TAG_commCon,commCon);
-                hashMap.put(TAG_commDate,commDate);
-
-                MyCommentList.add(hashMap);
-            }
-            ListAdapter adapter = new SimpleAdapter(
-                    config_my_forum_forum.this, MyCommentList,R.layout.config_item_list,
-                    new String[]{TAG_notNum,TAG_commCon,TAG_commDate},
-                    new int[]{R.id.textView_list_config_notNum,R.id.textView_list_config_notTi,R.id.textView_list_config_notDate}
-            );
-            commentlist.setAdapter(adapter);
-        } catch (JSONException e) {
-            Log.d(TAG, "showResult :", e);
-        }
-    }
+    */
 
     @Override
     public void onClick(View v) {
@@ -353,13 +368,6 @@ public class config_my_forum_forum extends AppCompatActivity implements View.OnC
             Intent intent6 = new Intent(config_my_forum_forum.this, configActivity.class);
             startActivity(intent6);
         }
-        if (v.getId() == R.id.button_myNotice){//내 게시글보기 버튼클릭시 내가쓴 글 리스트 표시
-                clickednum = true;
-             }
-        if (v.getId() == R.id.button_myComment){
-            clickednum = false;
-        }
-
     }
 
 }
