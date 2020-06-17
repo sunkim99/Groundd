cf36d357d0f3c6c1b4cf1a2664f719cf043ebc83
brisela <kretixe@gmail.com>
2020-06-17 21:59:21 +0900

    아무거나

--- app/src/main/java/com/example/ground/forum_forum_in.java
+++ app/src/main/java/com/example/ground/forum_forum_in.java
@@ -30,12 +30,9 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 //게시판에서 게시물을 눌렀을때 해당게시물 보여지는화면
 
